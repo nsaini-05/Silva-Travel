706dbaecfea50dbc32acc23aea30fd7ee0a8cadd
nsaini-05 <asaini7788@gmail.com>
2023-07-28 21:33:01 -0600

    Edit Media Query

--- backend-travel/server.js
+++ backend-travel/server.js
@@ -1,22 +1,25 @@
-import express from "express"
-import cors from 'cors'
-import { getFavorites,getFavoriteDescription } from "./controllers/favoriteController.js"
-const app =express()
-app.use(cors())
-const port = 5000
+import express from "express";
+import cors from "cors";
+import {
+  getFavorites,
+  getFavoriteDescription,
+} from "./controllers/favoriteController.js";
+const app = express();
+app.use(cors());
+const port = 3000;
 
-app.get("/api/favorites",(req,res)=>{
-    getFavorites(req,res)
-})
+app.get("/api/favorites", (req, res) => {
+  getFavorites(req, res);
+});
 
-app.get("/api/favorites/:cityname", (req,res)=>{
-    getFavoriteDescription(req,res)
-})
+app.get("/api/favorites/:cityname", (req, res) => {
+  getFavoriteDescription(req, res);
+});
 
 app.use((err, req, res, next) => {
-    res.status(500).json({ error: 'Something went wrong' });
-  });
+  res.status(500).json({ error: "Something went wrong" });
+});
 
-app.listen(port ,()=>{
-    console.log("Server started 300")
-})
+app.listen(port, () => {
+  console.log("Server started 300");
+});
--- frontend-travel/src/app/services/backend.service.ts
+++ frontend-travel/src/app/services/backend.service.ts
@@ -1,4 +1,4 @@
-import { Injectable , EventEmitter } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BackendApiResponse } from '../dataTypes';
 
@@ -6,25 +6,23 @@ import { BackendApiResponse } from '../dataTypes';
   providedIn: 'root',
 })
 export class BackendService {
-  constructor(private http : HttpClient) {}
-  apiUrl = "http://127.0.0.1:5000/api/favorites/"
+  constructor(private http: HttpClient) {}
+  apiUrl = 'http://127.0.0.1:3000/api/favorites/';
   backendApiServiceStatus = new EventEmitter<Object>();
-   getFavoriteCities() {
-     return this.http.get(this.apiUrl)
+  getFavoriteCities() {
+    return this.http.get(this.apiUrl);
   }
 
-  getCityDescriptionByName(cityName:string){
-    this.http.get(this.apiUrl+cityName).subscribe((val:any)=>{
-      if(val){
+  getCityDescriptionByName(cityName: string) {
+    this.http.get(this.apiUrl + cityName).subscribe((val: any) => {
+      if (val) {
         this.backendApiServiceStatus.emit({
           data: val,
           loading: false,
           error: '',
-        })
-      }else{
-          
-      } 
-    })
+        });
+      } else {
+      }
+    });
   }
-
 }
